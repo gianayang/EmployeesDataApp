2fefd791d9f0a2e3cf11e9de2660a3112f954b54
gianayang <gianayang31@gmail.com>
2023-03-29 14:11:58 -0400

    first commit

---
+++ src/main/java/com/api/employeesdata/EmployeesDataApplication.java
@@ -0,0 +1,13 @@
+package com.api.employeesdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/employeesdata/EmployeesDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.employeesdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
